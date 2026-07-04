cc92ed97ef4129f6f9337ce359cba8085112d785
Tutankafer <127450427+Fet-bleh@users.noreply.github.com>
2026-07-04 01:31:51 -0600

    name+basemaquina
    polnito

---
+++ OpenMachineState.gd
@@ -0,0 +1,18 @@
+extends CollisionShape2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+
+func _on_area_maquina_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		
---
+++ main.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var hand_cursor
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+#mouse de mano cuando entra en un area, conectar cuando cree un objeto ##obj.mouse_entered.connect(area2d_mouse_enter)
+func area2d_mouse_enter():
+	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+
+#mouse de mano cuando sale de un area, conectar cuando cree un objeto  ##obj.mouse_exited.connect(area2d_mouse_exit)
+func area2d_mouse_exit():
+	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
